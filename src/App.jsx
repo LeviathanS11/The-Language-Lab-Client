dd1d2dadeaba6bde4dabe3cc521510247bb6c7ea
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-06-07 02:06:34 +0600

    basic router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <h1 className='text-red-400'>this is assignment 12</h1>
+      <button className="btn btn-outline btn-primary w-28">Buy Now</button>
     </div>
   )
 }
