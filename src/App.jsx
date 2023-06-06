326d17441ce0697af7e32a6527142b7085d211a6
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-06-06 23:30:01 +0600

    starting assignment-12

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-red-400'>this is assignment 12</h1>
+    </div>
+  )
+}
+
+export default App
