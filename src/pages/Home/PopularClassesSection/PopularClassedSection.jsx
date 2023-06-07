8e952ce383e569e391a81d72c16b48dd0b35b506
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-06-07 22:41:56 +0600

    swiper js, classes json

---
+++ src/pages/Home/PopularClassesSection/PopularClassedSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PopularClassedSection = () => {
+    return (
+        <div>
+            <h1>this is Popular class section</h1>
+        </div>
+    );
+};
+
+export default PopularClassedSection;
